8f0447b3e9f90d69059668ff22dc69b8a2733b9e
xuyuan <xuyuan@gmail.com>
2022-05-26 23:12:00 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,33 @@
  * @Author: 大侠传授两招吧
  * @Date: 2022-05-25 21:08:33
  * @LastEditors: 大侠传授两招吧
- * @LastEditTime: 2022-05-26 22:41:21
+ * @LastEditTime: 2022-05-26 23:11:18
  * @Description: 
 -->
 <template>
-    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-    <van-button type="primary" @click="reducer">主要按钮</van-button>
-    <img alt="Vue logo" src="@/assets/images/login-bg.png" />
-    <img src="@img/login-bg.png" alt="">
+    <router-view
+        v-slot="{ Component }"
+    >
+        <!-- <transition appear :name="transitionName"> -->
+        <keep-alive>
+            <component :is="Component" />
+        </keep-alive>
+        <!-- </transition> -->
+    </router-view>
 </template>
 
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { computed, watch, onMounted } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { onMounted } from 'vue';
 import { useStore } from '@/store';
 import { CONFIG_ACTIONS } from '@/store/actions/config.actions';
- 
-// import { MUTATIONS } from '@/store/mutations-types';
 
 const store = useStore();
-const token = computed(() => store.state.config.token);
-const config = computed(() => store.state.config.setting);
-
-const reducer = () => {
-    store.dispatch(CONFIG_ACTIONS.FETCH_CONFIG_ACTION, 'ssssssssss');
-}
 
 onMounted(() => {
-    store.dispatch(CONFIG_ACTIONS.FETCH_CONFIG_ACTION, 'aaaaaaaaaaa')
+    store.dispatch(CONFIG_ACTIONS.FETCH_CONFIG_ACTION, 'aaaaaaaaaaa');
 });
-
-watch(() => config.value, (val) => {
-    console.log(val);
-})
 </script>
 
 <style lang="scss">
@@ -44,9 +36,6 @@ watch(() => config.value, (val) => {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
-    color: $red;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: 大侠传授两招吧
+ * @Date: 2022-05-26 23:08:26
+ * @LastEditors: 大侠传授两招吧
+ * @LastEditTime: 2022-05-26 23:08:31
+ * @Description: 
+-->
+<template>
+    <div class="login">
+
+   </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.login{
+
+}
+</style>
